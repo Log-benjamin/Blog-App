da7236dc10bebfc82c5c524b432676d6f874d7e9
binyamT <binyampowerhc@gmail.com>
2023-10-05 02:45:24 +0300

    fix postgres error

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -21,27 +21,27 @@ RSpec.describe 'Users', type: :request do
       get users_path
       expect(response.body).to include('This page contains a list of Users')
     end
+  end
 
-    describe 'GET /users/456' do
-      it 'Must render a successful response' do
-        get users_path(456)
-        expect(response).to have_http_status(:success)
-      end
+  describe 'GET /users/456' do
+    it 'Must render a successful response' do
+      get users_path(456)
+      expect(response).to have_http_status(:success)
+    end
 
-      it 'Must return a 200 OK status' do
-        get '/users/456'
-        expect(response).to have_http_status(200)
-      end
+    it 'Must return a 200 OK status' do
+      get '/users/456'
+      expect(response).to have_http_status(200)
+    end
 
-      it 'Must render the index template' do
-        get '/users/456'
-        expect(response).to render_template(:show)
-      end
+    it 'Must render the index template' do
+      get '/users/456'
+      expect(response).to render_template(:show)
+    end
 
-      it 'Must have a correct placeholder text in the response body' do
-        get '/users/456'
-        expect(response.body).to include('More details of a specific user')
-      end
+    it 'Must have a correct placeholder text in the response body' do
+      get '/users/456'
+      expect(response.body).to include('More details of a specific user')
     end
   end
 end
